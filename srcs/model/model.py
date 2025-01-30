7345ea11ed0eab0e8f76cecbcc3f3dcd824e5c60
Julia Osnovskaia <78608364+jul-os@users.noreply.github.com>
2025-01-31 02:20:09 +0700

    Update model.py to resnet
    decided to try and take a pretrained model

--- srcs/model/model.py
+++ srcs/model/model.py
@@ -1,8 +1,7 @@
+import torchvision.models as models
 import torch
-import torchvision
 
-
-def get_mobilenet_model(num_classes: int = 29):
-    model = torchvision.models.mobilenet_v3_small(weights=torchvision.models.mobilenet.MobileNet_V3_Small_Weights)
-    model.classifier[-1] = torch.nn.Linear(model.classifier[-1].in_features, num_classes)
+def get_resnet_18_model(num_classes: int = 29, pretrained: bool = True):
+    model = models.resnet18(pretrained=pretrained)
+    model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
     return model
