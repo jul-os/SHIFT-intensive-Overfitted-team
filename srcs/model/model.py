8763fa77fcff24268042e1ad26879e4ff4c67bd2
Julia Osnovskaia <78608364+jul-os@users.noreply.github.com>
2025-01-31 18:03:16 +0700

    Update model.py

--- srcs/model/model.py
+++ srcs/model/model.py
@@ -9,7 +9,7 @@ import torchvision
 
 
 def get_resnet_18_model(num_classes: int = 29, pretrained: bool = True):
-    model = models.resnet18(pretrained=pretrained)
+    model = torchvision.models.resnet18(pretrained=pretrained)
     model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
     return model
 
