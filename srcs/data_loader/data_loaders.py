d3cb7cb6a66490ab3187c7aa99708a87622f1d2d
Semyon Borodin <s.borodin@g.nsu.ru>
2025-01-31 22:57:56 +0700

    Update data_loaders.py
    pad added.
    Example:
    
    Pic 1000x2000 -> 2000x2000 -> 200x200 (Resize)

--- srcs/data_loader/data_loaders.py
+++ srcs/data_loader/data_loaders.py
@@ -9,14 +9,39 @@ from torch.utils.data import DataLoader, Dataset
 
 from torchvision import transforms
 
-#аугментации
+def pad_to_square(img):
+    width, height = img.size
+    max_size = max(width, height)
+    
+    pad_left = (max_size - width) // 2
+    pad_top = (max_size - height) // 2
+    pad_right = max_size - width - pad_left
+    pad_bottom = max_size - height - pad_top
+    
+    return transforms.Pad((pad_left, pad_top, pad_right, pad_bottom), fill=0)(img)
+
+
+def to_square(img):
+    width, height = img.size
+    max_size_of_pic = max(width, height)
+
+    pad_left = (max_size_of_pic - width)//2
+    pad_top = (max_size_of_pic - height)//2
+    pad_right = max_size_of_pic - width - pad_left
+    pad_bottom = max_size_of_pic - height - pad_top
+
+    return transforms.Pad((pad_left, pad_top, pad_right, pad_bottom), fill=0)(img)
+
+
+# аугментации
 transform = transforms.Compose([
-                            transforms.ColorJitter(brightness=0.3, contrast=0.3,
-                                                   saturation=0.15, hue=(-0.1, 0.1)),
-                            transforms.RandomPerspective(distortion_scale=0.2, p=0.3),
-                            transforms.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 2.0)),
-                            transforms.Resize((200, 200)),
-                            transforms.ToTensor()])
+    transforms.ColorJitter(brightness=0.3, contrast=0.3,
+                           saturation=0.15, hue=(-0.1, 0.1)),
+    transforms.RandomPerspective(distortion_scale=0.2, p=0.3),
+    transforms.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 2.0)),
+    transforms.Lambda(to_square),
+    transforms.Resize(200, interpolation=transforms.InterpolationMode.BICUBIC),
+    transforms.ToTensor()])
 
 class SignDataset(Dataset):
     def __init__(self, paths: List[Path], transform=None):
